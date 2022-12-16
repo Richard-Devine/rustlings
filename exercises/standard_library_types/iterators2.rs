0e1e09c4a5963b4f2fae24a7783d419e4e87d580
Richard Devine <richard_devine@outlook.com>
2022-12-16 14:56:21 +0000

    initial

--- exercises/standard_library_types/iterators2.rs
+++ exercises/standard_library_types/iterators2.rs
@@ -3,8 +3,6 @@
 // can offer. Follow the steps to complete the exercise.
 // Execute `rustlings hint iterators2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 // Step 1.
 // Complete the `capitalize_first` function.
 // "hello" -> "Hello"
@@ -12,7 +10,7 @@ pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
     match c.next() {
         None => String::new(),
-        Some(first) => ???,
+        Some(character) => character.to_uppercase().collect::<String>() + c.as_str()
     }
 }
 
@@ -21,7 +19,11 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+    let mut newWords = vec![];
+    for word in words.iter(){
+        newWords.push(capitalize_first(word))
+    }
+    newWords
 }
 
 // Step 3.
@@ -29,7 +31,11 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+    let mut newWords = String::new();
+    for word in words.iter(){
+        newWords = newWords + &capitalize_first(word);
+    }
+    newWords
 }
 
 #[cfg(test)]
--- exercises/standard_library_types/iterators4.rs
+++ exercises/standard_library_types/iterators4.rs
@@ -1,9 +1,9 @@
 // iterators4.rs
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 pub fn factorial(num: u64) -> u64 {
+    (1..=num).fold(1, |acc, v | acc * v)
+    //*vec!(0; num as usize).iter().enumerate().map(|enumerable| num - enumerable.0 as u64).collect::<Vec<u64>>().iter().reduce(|a,b| a * b).unwrap()
     // Complete this function to return the factorial of num
     // Do not use:
     // - return
